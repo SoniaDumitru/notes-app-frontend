47b5961fc7168056b3e8861c54e9c3c242a13f78
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-07 13:17:57 -0600

    patch request

--- src/app.js
+++ src/app.js
@@ -3,7 +3,26 @@ class App {
       document.querySelector('#notes-list').addEventListener('click', e => {
         const id = parseInt(e.target.dataset.id);
         const note = Note.findById(id);
-        document.querySelector('#update').innerHTML = note.renderUpdateForm();
+        document.querySelector('#update').addEventListener('submit', e => {
+            e.preventDefault();
+            const id = parseInt(e.target.dataset.id);
+            const note = Note.findById(id);
+            const title = e.target.querySelector('input').value;
+            const content = e.target.querySelector('textarea').value;
+      
+            const bodyJSON = { title, content };
+            fetch(`http://localhost:3000/api/v1/notes/${note.id}`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+              body: JSON.stringify(bodyJSON),
+            })
+              .then(res => res.json())
+              // our backend responds with the updated note instance represented as JSON
+              .then(updatedNote => console.log(updatedNote));
+          });
       });
     }
   }
