3fe0888bf332f32f054b61f08637a0bf9c5a866e
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-07 12:37:36 -0600

    added script for note.js to wire it up

--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(res => res.json())
       .then(json =>
         json.forEach(note => {
-          const markup = `
-            <li>
-                <h3>${note.title}
-                <button>edit</button>
-                </h3>
-            </li>`;
-          document.querySelector('#notes-list').innerHTML += markup;
+          const newNote = new Note(note);
+          document.querySelector('#notes-list').innerHTML += newNote.renderListItem();
         })
       );
   });
